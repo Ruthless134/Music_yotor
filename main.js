6d8264ce3790330e92f89e876cb58095d4a5fb41
Ruthless134 <148127140+Ruthless134@users.noreply.github.com>
2024-08-03 21:06:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,190 +1,126 @@
-$(document).ready(function() {
-    const song = $("#song")[0];
-    const controlIcon = $("#controlIcon");
-    const forwardButton = $(".fa-forward");
-    const backwardButton = $(".fa-backward");
-    const songTitle = $("#songTitle");
-    const artistName = $("#artistName");
-    const progressDot = $("#progressDot");
-    const progressLine = $("#progressLine");
+const box = document.querySelector(".box"),
+        musicImg = box.querySelector(".img-area img"),
+        musicName = box.querySelector(".name"),
+        musicArtist = box.querySelector(".artist"),
+        playPauseBtn = box.querySelector("#playPause"),
+        prevBtn = box.querySelector("#prev"),
+        nextBtn = box.querySelector("#next"),
+        mainAudio = box.querySelector("#main-audio"),
+        progressArea = box.querySelector(".progress-area"),
+        progressBar = box.querySelector(".progress-bar");
 
-    const songsList = [
+      const allMusic = [
         {
-            title: "Redemption",
-            name: "Besomorph & Coopex",
-            source: "https://github.com/ecemgo/mini-samples-great-tricks/blob/main/song-list/Besomorph-Coopex-Redemption.mp3?raw=true",
-            cover: "https://i.ytimg.com/vi/Z8mWqjfvg-A/maxresdefault.jpg",
+          name: "Redemption",
+          artist: "Besmorph & Coopex",
+          src: "https://github.com/ecemgo/mini-samples-great-tricks/blob/main/song-list/Besomorph-Coopex-Redemption.mp3?raw=true",
+          img: "https://i.ytimg.com/vi/Z8mWqjfvg-A/maxresdefault.jpg",
         },
         {
-            title: "What's the Problem?",
-            name: "OSKI",
-            source: "https://github.com/ecemgo/mini-samples-great-tricks/blob/main/song-list/OSKI-Whats-The-Problem.mp3?raw=true",
-            cover: "https://linkstorage.linkfire.com/medialinks/images/fd2634bd-07c6-4656-b970-843038c78b51/artwork-440x440.jpg",
+          name: "What's the Problem?",
+          artist: "OSKI",
+          src: "https://github.com/ecemgo/mini-samples-great-tricks/blob/main/song-list/OSKI-Whats-The-Problem.mp3?raw=true",
+          img: "https://linkstorage.linkfire.com/medialinks/images/fd2634bd-07c6-4656-b970-843038c78b51/artwork-440x440.jpg",
         },
         {
-            title: "Control",
-            name: "Unknown Brain x Rival",
-            source: "https://github.com/ecemgo/mini-samples-great-tricks/blob/main/song-list/Unknown-BrainxRival-Control.mp3?raw=true",
-            cover: "https://i.ytimg.com/vi/d455TYstD_w/maxresdefault.jpg",
-        }
-    ];
+          name: "Control",
+          artist: "Unknown Brain x Rival",
+          src: "https://github.com/ecemgo/mini-samples-great-tricks/blob/main/song-list/Unknown-BrainxRival-Control.mp3?raw=true",
+          img: "https://i.ytimg.com/vi/d455TYstD_w/maxresdefault.jpg",
+        },
+      ];
+      let musicIndex = Math.floor(Math.random() * allMusic.length);
+      let isMusicPaused = true;
 
-    let currentSongIndex = 0;
+      window.addEventListener("load", () => {
+        loadMusic(musicIndex);
+      });
 
-    function updateSongInfo() {
-        songTitle.text(songsList[currentSongIndex].title);
-        artistName.text(songsList[currentSongIndex].name);
-        song.src = songsList[currentSongIndex].source;
-    }
+      function loadMusic(indexNumb) {
+        musicName.innerText = allMusic[indexNumb].name;
+        musicArtist.innerText = allMusic[indexNumb].artist;
+        musicImg.src = allMusic[indexNumb].img;
+        mainAudio.src = allMusic[indexNumb].src;
+      }
 
-    function updateProgress() {
-        const progressValue = (song.currentTime / song.duration) * 100;
-        const lineWidth = progressLine.width();
-        const dotPosition = (progressValue / 100) * lineWidth;
-        progressDot.css('left', dotPosition + 'px');
-    }
+      function playMusic() {
+        box.classList.add("paused");
+        musicImg.classList.add("rotate");
+        playPauseBtn.innerHTML = '<i class="fas fa-pause"></i>';
+        mainAudio.play();
+        isMusicPaused = false;
+      }
 
-    controlIcon.on("click", function() {
-        if (song.paused) {
-            song.play();
-            controlIcon.removeClass("fa-play").addClass("fa-pause");
+      function pauseMusic() {
+        box.classList.remove("paused");
+        musicImg.classList.remove("rotate");
+        playPauseBtn.innerHTML = '<i class="fas fa-play"></i>';
+        mainAudio.pause();
+        isMusicPaused = true;
+      }
+
+      playPauseBtn.addEventListener("click", () => {
+        if (isMusicPaused) {
+          playMusic();
         } else {
-            song.pause();
-            controlIcon.removeClass("fa-pause").addClass("fa-play");
+          pauseMusic();
         }
-    });
+      });
 
-    forwardButton.on("click", function() {
-        currentSongIndex = (currentSongIndex + 1) % songsList.length;
-        updateSongInfo();
-        song.play();
-        controlIcon.removeClass("fa-play").addClass("fa-pause");
-    });
+      function prevMusic() {
+        musicIndex--;
+        if (musicIndex < 0) musicIndex = allMusic.length - 1;
+        loadMusic(musicIndex);
+        playMusic();
+      }
 
-    backwardButton.on("click", function() {
-        currentSongIndex = (currentSongIndex - 1 + songsList.length) % songsList.length;
-        updateSongInfo();
-        song.play();
-        controlIcon.removeClass("fa-play").addClass("fa-pause");
-    });
+      function nextMusic() {
+        musicIndex++;
+        if (musicIndex >= allMusic.length) musicIndex = 0;
+        loadMusic(musicIndex);
+        playMusic();
+      }
 
-    song.addEventListener('timeupdate', updateProgress);
+      prevBtn.addEventListener("click", () => {
+        prevMusic();
+      });
 
-    progressDot.on('click', function(e) {
-        const lineOffset = progressLine.offset().left;
-        const lineWidth = progressLine.width();
-        const clickPosition = e.pageX - lineOffset;
-        const progressValue = (clickPosition / lineWidth) * 100;
-        song.currentTime = (progressValue / 100) * song.duration;
-        updateProgress();
-    });
+      nextBtn.addEventListener("click", () => {
+        nextMusic();
+      });
 
-    updateSongInfo();
-});
+      mainAudio.addEventListener("timeupdate", (e) => {
+        const currentTime = e.target.currentTime;
+        const duration = e.target.duration;
+        let progressWidth = (currentTime / duration) * 100;
+        progressBar.style.width = `${progressWidth}%`;
 
-// $(document).ready(function() {
-//     const progress = $("#progress");
-//     const song = $("#song")[0];
-//     const controlIcon = $("#controlIcon");
-//     const forwardButton = $(".fa-forward");
-//     const backwardButton = $(".fa-backward");
-//     const rotatingImage = $("#rotatingImage")[0];
-//     const songName = $(".music-player h2");
-//     const artistName = $(".music-player p");
+        let musicCurrentTime = box.querySelector(".current-time");
+        let musicDuration = box.querySelector(".max-duration");
 
-//     const songsList = [
-//         {
-//             title: "Redemption",
-//             name: "Besmorph & Coopex",
-//             source: "https://github.com/ecemgo/mini-samples-great-tricks/blob/main/song-list/Besomorph-Coopex-Redemption.mp3?raw=true",
-//             cover: "https://i.ytimg.com/vi/Z8mWqjfvg-A/maxresdefault.jpg",
-//         },
-//         {
-//             title: "What's the Problem?",
-//             name: "OSKI",
-//             source: "https://github.com/ecemgo/mini-samples-great-tricks/blob/main/song-list/OSKI-Whats-The-Problem.mp3?raw=true",
-//             cover: "https://linkstorage.linkfire.com/medialinks/images/fd2634bd-07c6-4656-b970-843038c78b51/artwork-440x440.jpg",
-//         },
-//         {
-//             title: "Control",
-//             name: "Unknown Brain x Rival",
-//             source: "https://github.com/ecemgo/mini-samples-great-tricks/blob/main/song-list/Unknown-BrainxRival-Control.mp3?raw=true",
-//             cover: "https://i.ytimg.com/vi/d455TYstD_w/maxresdefault.jpg",
-//         }
-//     ];
+        mainAudio.addEventListener("loadeddata", () => {
+          let mainAdDuration = mainAudio.duration;
+          let totalMin = Math.floor(mainAdDuration / 60);
+          let totalSec = Math.floor(mainAdDuration % 60);
+          if (totalSec < 10) totalSec = "0" + totalSec;
+          musicDuration.innerText = `${totalMin}:${totalSec}`;
+        });
 
-//     let currentSongIndex = 0;
+        let currentMin = Math.floor(currentTime / 60);
+        let currentSec = Math.floor(currentTime % 60);
+        if (currentSec < 10) currentSec = "0" + currentSec;
+        musicCurrentTime.innerText = `${currentMin}:${currentSec}`;
+      });
 
-//     function updateSongInfo() {
-//         songName.text(songsList[currentSongIndex].title);
-//         artistName.text(songsList[currentSongIndex].name);
-//         song.src = songsList[currentSongIndex].source;
-//         rotatingImage.src = songsList[currentSongIndex].cover;
-//         song.load();
-//     }
+      progressArea.addEventListener("click", (e) => {
+        let progressWidth = progressArea.clientWidth;
+        let clickedOffsetX = e.offsetX;
+        let songDuration = mainAudio.duration;
 
-//     updateSongInfo();
+        mainAudio.currentTime = (clickedOffsetX / progressWidth) * songDuration;
+        playMusic();
+      });
 
-//     function playPause() {
-//         if (song.paused) {
-//             song.play();
-//             controlIcon.addClass("fa-pause").removeClass("fa-play");
-//             startRotation();
-//         } else {
-//             song.pause();
-//             controlIcon.addClass("fa-play").removeClass("fa-pause");
-//             pauseRotation();
-//         }
-//     }
-
-//     $("#controlIcon").click(playPause);
-
-//     let rotating = false;
-//     let currentRotation = 0;
-//     let rotationInterval;
-
-//     function rotateImage() {
-//         currentRotation += 1;
-//         rotatingImage.style.transform = `rotate(${currentRotation}deg)`;
-//     }
-
-//     function startRotation() {
-//         if (!rotating) {
-//             rotating = true;
-//             rotationInterval = setInterval(rotateImage, 50);
-//         }
-//     }
-
-//     function pauseRotation() {
-//         rotating = false;
-//         clearInterval(rotationInterval);
-//     }
-
-//     song.addEventListener("timeupdate", function() {
-//         if (!song.paused) {
-//             progress.val(song.currentTime);
-//         }
-//     });
-
-//     progress.on("input", function() {
-//         song.currentTime = progress.val();
-//     });
-
-//     song.addEventListener("ended", function() {
-//         currentSongIndex = (currentSongIndex + 1) % songsList.length;
-//         updateSongInfo();
-//         playPause();
-//     });
-
-//     forwardButton.click(function() {
-//         currentSongIndex = (currentSongIndex + 1) % songsList.length;
-//         updateSongInfo();
-//         if (!song.paused) playPause();
-//     });
-
-//     backwardButton.click(function() {
-//         currentSongIndex = (currentSongIndex - 1 + songsList.length) % songsList.length;
-//         updateSongInfo();
-//         if (!song.paused) playPause();
-//     });
-// });
+      mainAudio.addEventListener("ended", () => {
+        nextMusic();
+      });
